40982d8407ce76a000461a4afde7e2430760e249
Ariel-HS <arielherfrison@gmail.com>
2024-02-10 09:58:10 +0700

    feat: added solve command

--- src/tucil_gui.py
+++ src/tucil_gui.py
@@ -4,6 +4,107 @@ import random
 import tkinter as tk
 from tkinter import * 
 
+def func():
+    buffer_size = int(buffer_entry.get())
+    matrix = matrix_entry.get('1.0','end-1c').split("\n")
+    matrix = [line.split() for line in matrix]
+    height = len(matrix)
+    width = len(matrix[0])
+    sequences = sequence_entry.get('1.0','end-1c').split("\n")
+    sequences = [line.split() for line in sequences]
+    sequences_amount = len(sequences)
+    sequences_reward = [1 for i in range(sequences_amount)]
+
+    matrix_2 = [[1 for i in range(width)] for j in range(height)]
+
+    def checkReward(buffer):
+        reward = 0
+        for i in range(sequences_amount):
+            # print(buffer)
+            # print(sequences[i])
+            has_reward = False
+            j = 0
+            while not has_reward and j < (len(buffer)-len(sequences[i])+1):
+                is_reward = True
+                k = 0
+                while is_reward and k < len(sequences[i]): 
+                    if (buffer[j+k] != sequences[i][k]):
+                        is_reward = False
+                    k += 1
+                if is_reward:
+                    reward += sequences_reward[i]
+                    has_reward = True
+                j += 1
+
+        return reward
+
+    def horizontal(buffer_size,row,ctr,buffer,coor_buffer):
+        # print(buffer,coor_buffer)
+        max_reward = checkReward(buffer)
+        max_buffer = buffer
+        max_coor = coor_buffer
+        if (ctr < buffer_size):
+
+            for i in range (width):
+                if (matrix_2[row][i] != 0):
+                    matrix_2[row][i] = 0
+                    buffer.append(matrix[row][i])
+                    coor_buffer.append(str(i+1)+','+str(row+1))
+                    new_buffer = buffer.copy()
+                    new_coor_buffer = coor_buffer.copy()
+                    new_reward = vertical(buffer_size,i,ctr+1,new_buffer,new_coor_buffer)
+                    buffer.pop()
+                    coor_buffer.pop()
+                    matrix_2[row][i] = 1
+
+                    if new_reward[0] > max_reward:
+                        max_reward = new_reward[0]
+                        max_buffer = new_reward[1]
+                        max_coor = new_reward[2]
+        
+        return (max_reward,max_buffer,max_coor)
+
+    def vertical(buffer_size,column,ctr,buffer,coor_buffer):
+        # print(buffer,coor_buffer)
+        max_reward = checkReward(buffer)
+        max_buffer = buffer
+        max_coor = coor_buffer
+        if (ctr < buffer_size):
+
+            for i in range(height):
+                if (matrix_2[i][column] != 0):
+                    matrix_2[i][column] = 0
+                    buffer.append(matrix[i][column])
+                    coor_buffer.append(str(column+1)+','+str(i+1))
+                    new_buffer = buffer.copy()
+                    new_coor_buffer = coor_buffer.copy()
+                    new_reward = horizontal(buffer_size,i,ctr+1,new_buffer,new_coor_buffer)
+                    buffer.pop()
+                    coor_buffer.pop()
+                    matrix_2[i][column] = 1
+
+                    if new_reward[0] > max_reward:
+                        max_reward = new_reward[0]
+                        max_buffer = new_reward[1]
+                        max_coor = new_reward[2]
+
+        return (max_reward,max_buffer,max_coor)
+
+    print(buffer_size)
+    print(matrix)
+    print(sequences)
+
+    start = round(time.time()*1000)
+    max = horizontal(buffer_size,0,0,[],[])
+    print(max[0])
+    for tokens in max[1]:
+        print(tokens,end=" ")
+    print()
+    for coors in max[2]:
+        print(coors)
+    end = round(time.time()*1000)
+    print(f"\n{end-start} ms\n")
+    
 window = tk.Tk(className="python Cyberpunk 2077 Breach Protocol Solver")
 window.geometry("1024x576")
 window.configure(background="#16151b")
@@ -30,7 +131,7 @@ buffer_entry.pack(pady=10)
 button_border = tk.Frame(master=buffer,height=53,width=103,bg='#16151b',highlightbackground='#b3cc50',highlightcolor='#b3cc50',highlightthickness=3,bd=0)
 button_border.pack(expand=True,anchor='w',side='left')
 button_border.pack_propagate(False)
-solve_button = tk.Button(master=button_border,height=50,width=100,bg='#16151b',text='Solve',fg='#b3cc50',font='Bebas',activebackground='#606e0c',bd=0)
+solve_button = tk.Button(master=button_border,command=func,height=50,width=100,bg='#16151b',text='Solve',fg='#b3cc50',font='Bebas',activebackground='#606e0c',bd=0)
 solve_button.pack(expand=True)
 
 matrix = tk.Frame(master=window,bg='#16151b')
